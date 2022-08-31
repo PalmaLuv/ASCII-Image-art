623dee860d532110665bf595c19dd7858a8d879f
Palma.luv <84909252+Palma-IT-DUO@users.noreply.github.com>
2022-08-31 03:15:36 +0300

    add file programs

---
+++ ASCII Image art/Config.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ASCII
+{
+    public class ConfigConsole
+    {
+        public int MAXsize {get; set;}
+        public ConfigConsole(int size)
+        {
+            this.MAXsize = size; 
+        }
+    }
+
+    public static class Menu
+    {
+        public static void menu()
+        {
+            ConsoleKeyInfo Key; 
+            while(true)
+            {
+                Key = Console.ReadKey(true); 
+                if(Key.Key == ConsoleKey.D1)
+                {
+                    while(true)
+                    {
+                        Key = Console.ReadKey(true);
+                        if (Key.Key == ConsoleKey.O)
+                        {
+                            BitmapToASCII.d();
+                        }
+                        if (Key.Key == ConsoleKey.Escape)
+                            break;
+                    }    
+                }
+                if (Key.Key == ConsoleKey.Escape)
+                    break;
+            }
+        }
+
+        public static int ConfigEdit()
+        {
+            return 0;
+        }
+
+        public static string LanguageSelection()
+        {
+            return "en-EN";
+        }
+
+        public static string OpenFile()
+        {
+            var OpenFile = new OpenFileDialog
+            {
+                Filter = "Images | *.jpg; *.png; *.bmp; *.JPEG"
+            };
+            return OpenFile.FileName;
+        }
+
+        public static void SaveFile(string text)
+        {
+            Stream stream;
+            string LocalFilePath;
+            var SaveFile = new SaveFileDialog
+            {
+                Filter = "Text | *.txt"
+            };
+            
+            if (SaveFile.ShowDialog() == DialogResult.OK)
+                if ((stream = SaveFile.OpenFile())!= null)
+                {
+                    LocalFilePath = SaveFile.FileName.ToString();
+                    
+                    //FileName = LocalFilePath.Substring(LocalFilePath.LastIndexOf("\\") + 1);
+                    //FilePath = LocalFilePath.Substring(0, LocalFilePath.LastIndexOf("\\"));
+
+                    stream.Close();
+
+                    using (StreamWriter sw = new StreamWriter(LocalFilePath))
+                    {
+                        foreach (var result in text)
+                        {
+                            sw.Write(result);
+                        }
+                        sw.Close();
+                    };
+                }
+        }
+    }
+}
---
+++ ASCII Image art/ProcessingPhoto.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ASCII
+{
+    public static class ProcessingPhoto
+    {
+        public static void ConvertToGrad(this Bitmap bitmap)
+        {
+            for (int i = 0; i < bitmap.Height; i++)
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    var pixel = bitmap.GetPixel(i, j);
+                    int avg = (pixel.R + pixel.G + pixel.B) / 3;
+                    bitmap.SetPixel(j, i, Color.FromArgb(pixel.A, avg, avg, avg));
+                }
+        }
+
+        public static Bitmap ResizeBitmap(Bitmap bitmap, ConfigConsole cf)
+        {
+            var MAXWidth = cf.MAXsize;
+            var MAXHeight = bitmap.Height / 1.5 * MAXWidth / bitmap.Width;
+            if (bitmap.Width > MAXWidth || bitmap.Width > MAXHeight)
+                bitmap = new Bitmap(bitmap, new Size(MAXWidth, (int)MAXHeight));
+            return bitmap;
+        }
+
+
+    }
+
+    public class BitmapToASCII
+    {
+        public ConfigConsole cf = new ConfigConsole(150);
+
+        private readonly Bitmap bitmap;
+        private readonly string ASCII = " _.,-=+:;cba!?0123456789$W#@Ñ";
+        public BitmapToASCII(Bitmap bitmap)
+        {
+            this.bitmap = bitmap;
+        }
+
+        public void Procesing(string FileName)
+        {
+            var bitmap = new Bitmap(Menu.OpenFile());
+            bitmap = ResizeBitmap(bitmap, cf);
+            bitmap.ConvertToGrayscale();
+
+            var converter = new BitmapToASCIIConventer(bitmap);
+            var rows = converter.ConvertBitmap();
+
+            foreach (var row in rows)
+                Console.WriteLine(row);
+
+            Console.SetCursorPosition(0, 0);
+            string text = null;
+            try
+            {
+                for (int i = 0; i < rows.Length; i++)
+                    for (int j = 0; j < rows[i].Length; j++)
+                        text = rows[i][j].ToString();
+                Clipboard.SetText(text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private char[] ConvertASCII()
+        {
+            char[] result = new char[ASCII.Length];
+            for (int i = 0; i < ASCII.Length; i++)
+                result[i] = ASCII[i];
+            return result;
+        }
+        public char[][] ConvertBitmap()
+        {
+            var result = new char[bitmap.Height][];
+            for (int i = 0; i < bitmap.Height; i++) // it's "y" position
+            {
+                result[i] = new char[bitmap.Width];
+                for (int j = 0; j < bitmap.Width; j++) // it's "x" position
+                {
+                    int mapIndex = (int)Map(bitmap.GetPixel(j, i).R, 0, 255, 0, ASCII.Length - 1);
+                    result[i][j] = ConvertASCII()[mapIndex];
+                }
+            }
+            return result;
+        }
+
+        public float Map(float valueToMap , float start1 , float stop1 , float start2 , float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
